3278284b7fdfec7b918e40d3f2d95c8ffc37034b
shiva koka <siva.koka@xcubelabs.com>
2017-10-06 13:17:26 +0530

    Merge branch 'ramu' of https://bitbucket.org/shivakoka/connect-incon into shiva
    Conflicts:
    	app/src/main/java/com/incon/connect/ui/qrcodescan/QrcodeBarcodeScanActivity.java

--- app/src/main/java/com/incon/connect/ui/warrantyregistration/WarrantRegistrationPresenter.java
+++ app/src/main/java/com/incon/connect/ui/warrantyregistration/WarrantRegistrationPresenter.java
@@ -17,7 +17,6 @@ import io.reactivex.observers.DisposableObserver;
 /**
  * Created by PC on 9/26/2017.
  */
-
 public class WarrantRegistrationPresenter extends BasePresenter<WarrantRegistrationContract.View>
         implements WarrantRegistrationContract.Presenter {
 
